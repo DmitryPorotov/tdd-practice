2cdbc227e491a34d2ed16a309f642d1971dbd9c9
Dmitry <dmitryporotov@gmail.com>
2024-09-25 16:34:02 +0300

    finished mine sweeper grid

--- mine-sweeper/mine-grid.ts
+++ mine-sweeper/mine-grid.ts
@@ -1,13 +1,17 @@
 export default class MineGrid {
     private grid: Array<Uint8Array> = [];
     private playerLocation: number[] = [-1, -1];
-    constructor(width: number, heights: number, private mineProbability = 0.2) {
+    constructor(width: number = 8, heights: number = 8, private mineProbability = 0.2) {
         if (mineProbability >= 1) {
             throw new Error('Mine probability can\'t be 1.');
         }
         for (let i = 0; i < heights; i ++) {
             this.grid.push(new Uint8Array(8));
         }
+    }
+
+    get dimensions(): number[] {
+        return [this.grid[0].length, this.grid.length];
     }
 
     initMines() {
@@ -26,11 +30,71 @@ export default class MineGrid {
             }
         }
         if (!possibleStartPoints.length) {
-            throw new Error('Bottom row is all mines');
+            throw new NoPlaceForPlayerAtBottomRowException('Bottom row is all mines');
         }
         const pos = Math.floor(Math.random() * possibleStartPoints.length);
         this.playerLocation = [pos, this.grid.length - 1];
         return this.playerLocation;
+    }
+
+    getPlayerLocation(): number[] {
+        return [...this.playerLocation];
+    }
+
+    validate(): boolean {
+        const toVisit = [this.playerLocation];
+        const mines = this.getGridMines();
+        while (toVisit.length) {
+            let v: number[] = toVisit.pop() as number[];
+            if (v[1] === 0) {
+                return true
+            }
+            if (!this.isVisited(v)) {
+                this.visit(v);
+                if (v[1] > 0 && !mines[v[0]][v[1] - 1]) {
+                    toVisit.push([v[0], v[1] - 1]);
+                }
+                if (v[0] > 0 && !mines[v[0] - 1][v[1]]) {
+                    toVisit.push([v[0] - 1, v[1]]);
+                }
+                if (v[0] < this.grid[0].length - 1 && !mines[v[0] + 1][v[1]]) {
+                    toVisit.push([v[0] + 1, v[1]]);
+                }
+                if (v[1] < this.grid.length - 1 && !mines[v[0]][v[1] + 1]) {
+                    toVisit.push([v[0], v[1] + 1]);
+                }
+            }
+        }
+        return false;
+    }
+
+    resetVisited() {
+        for (const row of this.grid) {
+            for (let i = 0; i < row.length; i ++) {
+                row[i] = row[i] & ~2;
+            }
+        }
+        this.visit();
+    }
+
+    static deserialize(str: string): MineGrid {
+        const result = str.match(/.*\n/g);
+        if (!result)
+            throw new MalformedGridStringException('The string should have new line characters (\\n).');
+        const grid = new MineGrid(result[0].length - 1, result.length);
+        for (let i = 0; i < result.length; i++) {
+            for (let j = 0; j < result[i].length - 1; j++) {
+                let cell = parseInt(result[i][j]);
+                if (isNaN(cell)) throw new MalformedGridStringException(`Grid cell [${j},${i}] is not a number.`);
+                if (cell > 7) throw new MalformedGridStringException('A value of a cell can\'t be greater than 7.');
+                if (cell & 4) {
+                    cell &= ~4;
+                    grid.playerLocation = [j, i]
+                }
+                grid.grid[i][j] = cell
+            }
+        }
+        return grid
     }
 
     getGridMines(): boolean[][] {
@@ -44,4 +108,68 @@ export default class MineGrid {
         }
         return g;
     }
+
+    moveRight(): number[] {
+        if (this.playerLocation[0] < this.grid[0].length - 1) {
+            this.playerLocation[0] = this.playerLocation[0] + 1;
+            this.visit();
+        }
+        return this.playerLocation;
+    }
+
+    moveLeft(): number[] {
+        if (this.playerLocation[0] > 0) {
+            this.playerLocation[0] = this.playerLocation[0] - 1;
+            this.visit();
+        }
+        return this.playerLocation;
+    }
+
+    moveUp(): number[] {
+        if (this.playerLocation[1] > 0) {
+            this.playerLocation[1] = this.playerLocation[1] - 1;
+            this.visit();
+        }
+        return this.playerLocation;
+    }
+
+    moveDown(): number[] {
+        if (this.playerLocation[1] < this.grid.length - 1) {
+            this.playerLocation[1] = this.playerLocation[1] + 1;
+            this.visit();
+        }
+        return this.playerLocation;
+    }
+
+    visit(location?: number[]): void {
+        if (!location)
+            this.grid[this.playerLocation[1]][this.playerLocation[0]] |= 2;
+        else
+            this.grid[location[1]][location[0]] |= 2;
+    }
+
+    isVisited(location: number[]): boolean {
+        return !!(this.grid[location[1]][location[0]] & 2);
+    }
+
+    serialize(): string {
+        let n = 0;
+        let s = '';
+        for (const row of this.grid) {
+            for (let i = 0; i < row.length; i++) {
+                s += row[i] + (this.playerLocation[0] === i && this.playerLocation[1] === n ? 4 : 0)
+            }
+            s += '\n';
+            n ++;
+        }
+        return s;
+    }
+}
+
+export class NoPlaceForPlayerAtBottomRowException extends Error{
+
+}
+
+export class MalformedGridStringException extends Error{
+
 }
--- mine-sweeper/tests/mine-sweeper.spec.ts
+++ mine-sweeper/tests/mine-sweeper.spec.ts
@@ -1,5 +1,5 @@
-import {it, describe, expect, beforeAll} from '@jest/globals';
-import MineGrid from "../mine-grid";
+import {it, describe, expect, beforeEach} from '@jest/globals';
+import MineGrid, {NoPlaceForPlayerAtBottomRowException} from "../mine-grid";
 
 
 
@@ -35,10 +35,133 @@ describe('mine sweeper game', () => {
             expect(pos[0]).toBeGreaterThanOrEqual(0);
             expect(pos[0]).toBeLessThanOrEqual(7);
         } catch (e: unknown) {
-            if (e instanceof Error)
+            if (e instanceof NoPlaceForPlayerAtBottomRowException)
                 expect(e.message).toBe('Bottom row is all mines');
             else
                 throw e;
         }
     });
+    it('should be able to validate that there is a path from start to finish', () => {
+        const mineGrid = new MineGrid(8, 8, 0.2);
+        mineGrid.initMines();
+        while (true) {
+            try {
+              mineGrid.initPlayer();
+              break;
+            }
+            catch (e) {
+                if (!(e instanceof NoPlaceForPlayerAtBottomRowException)) {
+                    throw e;
+                }
+            }
+        }
+        expect(mineGrid.validate()).toBeTruthy()
+    })
+});
+
+describe("mine sweeper movement", () => {
+    let mineGrid: MineGrid;
+    beforeEach(() => {
+        mineGrid = new MineGrid(8, 8, 0.4);
+        mineGrid.initMines();
+        while (true) {
+            try {
+                mineGrid.initPlayer();
+                break;
+            }
+            catch (e) {
+                if (!(e instanceof NoPlaceForPlayerAtBottomRowException)) {
+                    throw e;
+                }
+            }
+        }
+    });
+    it('should be able to move the player right', () => {
+        const playerLoc = mineGrid.getPlayerLocation();
+        const newLoc = mineGrid.moveRight();
+        if (playerLoc[0] === mineGrid.dimensions[0] - 1) {
+            expect(newLoc[0]).toBe(playerLoc[0])
+        }
+        else {
+            expect(newLoc[0]).toBe(playerLoc[0] + 1)
+        }
+    });
+    it('should be able to move the player left', () => {
+        const playerLoc = mineGrid.getPlayerLocation();
+        const newLoc = mineGrid.moveLeft();
+        if (playerLoc[0] === 0) {
+            expect(newLoc[0]).toBe(playerLoc[0])
+        }
+        else {
+            expect(newLoc[0]).toBe(playerLoc[0] - 1)
+        }
+    });
+    it('should be able to move the player up', () => {
+        const playerLoc = mineGrid.getPlayerLocation();
+        const newLoc = mineGrid.moveUp();
+        if (playerLoc[1] === 0) {
+            expect(newLoc[1]).toBe(playerLoc[1])
+        }
+        else {
+            expect(newLoc[1]).toBe(playerLoc[1] - 1);
+        }
+    });
+    it('should be able to move the player down', () => {
+        const playerLoc = mineGrid.getPlayerLocation();
+        const newLoc = mineGrid.moveDown();
+        if (playerLoc[1] === mineGrid.dimensions[1] - 1) {
+            expect(newLoc[1]).toBe(playerLoc[1])
+        }
+        else {
+            expect(newLoc[1]).toBe(playerLoc[1] + 1);
+        }
+    });
+    it('should be able to check a location is visited', () => {
+        const playerLoc = mineGrid.getPlayerLocation();
+        mineGrid.visit();
+        const newLoc = mineGrid.moveUp();
+        expect(mineGrid.isVisited(playerLoc)).toBeTruthy();
+        expect(mineGrid.isVisited(newLoc)).toBeTruthy();
+        expect(mineGrid.isVisited([0,0])).toBeFalsy();
+    });
+});
+
+describe("mine sweeper serialization", () => {
+    it('should be able to serialize the grid to string', () => {
+        const mg = new MineGrid();
+        mg.initMines();
+        while (true) {
+            try {
+                mg.initPlayer();
+                break;
+            }
+            catch (e) {
+                if (!(e instanceof NoPlaceForPlayerAtBottomRowException)) {
+                    throw e;
+                }
+            }
+        }
+        const str = mg.serialize();
+        expect(typeof str).toBe('string');
+
+    });
+    it('should be able to deserialize a mine grid from string', () => {
+        const str =
+            "00100100\n" +
+            "00000100\n" +
+            "00101001\n" +
+            "00000010\n" +
+            "01000000\n" +
+            "00000000\n" +
+            "00000000\n" +
+            "04000001\n";
+        const mg = MineGrid.deserialize(str);
+        expect(mg.getPlayerLocation()).toStrictEqual([1,7]);
+        const gridMines = mg.getGridMines();
+        expect(gridMines[0][0]).toBeFalsy();
+        expect(gridMines[0][2]).toBeTruthy();
+        expect(gridMines[2][2]).toBeTruthy();
+        expect(gridMines[2][3]).toBeFalsy();
+
+    })
 });
